d8bee59b480dc25cdac3f977c1cdea3190c7d81f
Marcelo <727100@gmail.com>
2022-05-13 14:39:15 -0300

    Refatorando testes com page objects

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -1,11 +1,7 @@
+import SignupPage from '../pages/SignupPage'
 
 describe('cadastro', ()=>{
     it('Usuário deve se tornar um entregador', ()=>{
-        cy.viewport(640, 480)
-        cy.visit('https://buger-eats.vercel.app/')
-        
-        cy.get('a[href="/deliver"]').click()
-        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
 
         var deliver = {
             name: 'Marcelo',
@@ -24,43 +20,18 @@ describe('cadastro', ()=>{
             cnh: 'cnh-digital.jpg'
         }
 
-        cy.get('input[name="name').type(deliver.name)
-        cy.get('input[name="cpf').type(deliver.cpf)
-        cy.get('input[name="email').type(deliver.email)
-        cy.get('input[name="whatsapp').type(deliver.whatsapp)
+        var signup = new SignupPage()
 
-        cy.get('input[name="postalcode').type(deliver.address.postalcode)
-        cy.get('input[type=button][value="Buscar CEP"]').click()
-
-
-        cy.get('input[name="address-number').type(deliver.address.number)
-        cy.get('input[name="address-details').type(deliver.address.details)
-
-        cy.get('input[name="address"]').should('have.value', deliver.address.street)
-        cy.get('input[name="district"]').should('have.value', deliver.address.district)
-        cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
-
-        cy.contains('.delivery-method li', deliver.delivery_method).click()
-
-        cy.get('input[accept^="image"]').attachFile('/images/' + deliver.cnh)
-        /* expressões regulares */
-        /* inicia com 'imag' - 'input[accept^="imag"]'  */
-        /* termina com '/*' - 'input[accept$="/*"]'  */
-        /* contem 'age/' - 'input[accept*="age/"]'  */
-
-        cy.get('form button[type="submit"]').click()
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
 
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
-
-        cy.get('.swal2-container .swal2-html-container').should('have.text', expectedMessage)
+        signup.modalContentShouldBe(expectedMessage)
+        
     })
 
     it('CPF incorreto', ()=>{
-        cy.viewport(640, 480)
-        cy.visit('https://buger-eats.vercel.app/')
-        
-        cy.get('a[href="/deliver"]').click()
-        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
 
         var deliver = {
             name: 'Marcelo',
@@ -79,29 +50,14 @@ describe('cadastro', ()=>{
             cnh: 'cnh-digital.jpg'
         }
 
-        cy.get('input[name="name').type(deliver.name)
-        cy.get('input[name="cpf').type(deliver.cpf)
-        cy.get('input[name="email').type(deliver.email)
-        cy.get('input[name="whatsapp').type(deliver.whatsapp)
+        var signup = new SignupPage()
 
-        cy.get('input[name="postalcode').type(deliver.address.postalcode)
-        cy.get('input[type=button][value="Buscar CEP"]').click()
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
+        signup.alertMessageShouldBe('Oops! CPF inválido')
 
-
-        cy.get('input[name="address-number').type(deliver.address.number)
-        cy.get('input[name="address-details').type(deliver.address.details)
-
-        cy.get('input[name="address"]').should('have.value', deliver.address.street)
-        cy.get('input[name="district"]').should('have.value', deliver.address.district)
-        cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
-
-        cy.contains('.delivery-method li', deliver.delivery_method).click()
-
-        cy.get('input[accept^="image"]').attachFile('/images/' + deliver.cnh)
-
-        cy.get('form button[type="submit"]').click()
-
-        cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
+        
 
     })
 
